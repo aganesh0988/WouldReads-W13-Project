0782313d32777425474e15b689c6c51d9311d409
Daniel Ford <daniel.fordd@gmail.com>
2020-09-08 11:45:56 -0400

    expanding seeds

--- db/seeders/20200904223638-bookshelves-seed.js
+++ db/seeders/20200904223638-bookshelves-seed.js
@@ -3,6 +3,11 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Bookshelves', [
+      {shelfName: "Business", createdAt: new Date(), updatedAt: new Date()},
+      {shelfName: "School Books", createdAt: new Date(), updatedAt: new Date()},
+      {shelfName: "Classics", createdAt: new Date(), updatedAt: new Date()},
+      {shelfName: "Self Help Books", createdAt: new Date(), updatedAt: new Date()},
+      {shelfName: "Favorite Novels", createdAt: new Date(), updatedAt: new Date()},
     ])
   },
 
